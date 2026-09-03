bfab1020fc9015a23cec0391a0bed85ece119a1c
serjoleinic2-cloud <serjoleinic2@gmail.com>
2026-09-03 18:24:06 +0300

    feat: add leadClients to DashboardStats type

--- src/types/index.ts
+++ src/types/index.ts
@@ -161,7 +161,7 @@ export interface HistoryEntry {
 
 export interface Reminder {
   id: number;
-  client_id?: number;
+  client_id: number;
   client_name?: string;
   client_phone?: string | null;
   contract_number?: string | null;
@@ -190,6 +190,7 @@ export interface DashboardStats {
   atCustoms: number;
   inOffice: number;
   extrasCount: number;
+  leadClients: number;
 }
 
 export interface CarBrand {
